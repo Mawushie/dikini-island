0e0c0cdde69c5533e0bd53d1e8064de7ddf8e90c
Mawushie <afi.mawushie.honyah@gmail.com>
2022-11-22 13:49:43 +0000

    navbar added

--- src/pages/Index.js
+++ src/pages/Index.js
@@ -5,14 +5,22 @@ function Index() {
   return (
     <div>
       <div className="header">
-        <div>
+        <nav className="navbar">
+          <div className="container">
+            <a class="navbar-brand" href={<Index />}>
+              <img src="./images/logo1.png" alt="navlogo" />
+            </a>
+          </div>
+        </nav>
+
+        {/* <div>
           <img src="./images/logo2.png" />
-          {/* <div>
+          <div>
             <p className="header-text">
               Whatever Happens On The Island Stays On The Island
             </p>
-          </div> */}
-        </div>
+          </div>
+        </div> */}
       </div>
     </div>
   );
