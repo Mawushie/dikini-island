4cbf5728e07ce5e3a60a5be110622f9876982d30
Mawushie <afi.mawushie.honyah@gmail.com>
2022-11-22 11:38:45 +0000

    first commit

---
+++ src/pages/Index.js
@@ -0,0 +1,21 @@
+import React from "react";
+import "../sass/app.scss";
+
+function Index() {
+  return (
+    <div>
+      <div className="header">
+        <div>
+          {/* <img src="./assets/logo2.png" /> */}
+          {/* <div>
+            <p className="header-text">
+              Whatever Happens On The Island Stays On The Island
+            </p>
+          </div> */}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Index;
