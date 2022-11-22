f0cf2c4092aee3985c910da1583ffafb27f80e8b
Mawushie <afi.mawushie.honyah@gmail.com>
2022-11-22 12:01:40 +0000

    background image added

--- src/pages/Index.js
+++ src/pages/Index.js
@@ -6,7 +6,7 @@ function Index() {
     <div>
       <div className="header">
         <div>
-          {/* <img src="./assets/logo2.png" /> */}
+          <img src="./assets/logo2.png" />
           {/* <div>
             <p className="header-text">
               Whatever Happens On The Island Stays On The Island
