6f4ac78e8de9be2d74f4c32146bc61edac1bccb3
Mawushie <afi.mawushie.honyah@gmail.com>
2022-11-27 15:46:12 +0000

    confirmation page updated

--- src/pages/PaymentCorfirmed.js
+++ src/pages/PaymentCorfirmed.js
@@ -1,7 +1,19 @@
 import React from "react";
-
+import styled from "styled-components";
 function PaymentCorfirmed() {
-  return <div>Payment Corfirmed</div>;
+  return (
+    <Div>
+      <img src="./images/checked.png" />
+      <h2>Payment Confirmed</h2>
+    </Div>
+  );
 }
 
+const Div = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  margin: auto;
+`;
 export default PaymentCorfirmed;
